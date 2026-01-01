345f361388f2c28027325ee7513bd673bba88cc4
Kinan Sleman <kenan@oonatech.com>
2026-01-02 02:01:25 +0300

    query the database using supbase-js

---
+++ src/Dashboard.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import supabase from "./supabase-client";
+
+const Dashboard: React.FC = () => {
+    useEffect(() => {
+        const fetchMetrics = async () => {
+            const { data, error } = await supabase
+                .from("sales_deals")
+                .select("name, value")
+                .order("value", { ascending: false })
+                .limit(1);
+
+            if (error) {
+                console.error(error);
+            } else {
+                console.log("Top Sales Rep:", data);
+            }
+        };
+
+        fetchMetrics();
+    }, []);
+
+    return (
+        <div className="dashboard-wrapper">
+            <div className="chart-container">
+                <h2>Total Sales This Quarter ($)</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                Sales Team Dashboard
+            </h1>
+        </header >
+    )
+}
